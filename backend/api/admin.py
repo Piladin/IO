4ef300aadfb29d2b70d4caea5873101f440d6593
Wnek Mateusz-Piladin <endermanhunterpl@gmail.com>
2025-01-18 14:14:50 +0100

    admin browski

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Exercise, SystemUser
+from .models import Exercise, SystemUser, BrowsingHistory
 
 admin.site.register(SystemUser)
 admin.site.register(Exercise)
+admin.site.register(BrowsingHistory)
